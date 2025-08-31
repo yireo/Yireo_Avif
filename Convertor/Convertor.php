e9e2e6860bdb3ebb54f9ab453eb77e72075f092e
Jisse Reitsma <jisse@yireo.com>
2025-08-31 10:28:20 +0200

    Add proper support AVIF via GD even though this causes conversion issues most of the time

--- Convertor/Convertor.php
+++ Convertor/Convertor.php
@@ -2,174 +2,45 @@
 
 namespace Yireo\Avif\Convertor;
 
-use Magento\Framework\Exception\FileSystemException;
-use Magento\Framework\Filesystem\File\ReadFactory as FileReadFactory;
-use Magento\Framework\View\Asset\File\NotFoundException;
+use GdImage;
+use Throwable;
 use Yireo\NextGenImages\Convertor\ConvertorInterface;
 use Yireo\NextGenImages\Exception\ConvertorException;
-use Yireo\NextGenImages\Image\File;
-use Yireo\NextGenImages\Image\SourceImage;
-use Yireo\NextGenImages\Image\SourceImageFactory;
+use Yireo\NextGenImages\Image\Image;
 use Yireo\Avif\Config\Config;
-use Yireo\Avif\Image\ConvertWrapper;
+use Yireo\NextGenImages\Image\TargetImageFactory;
 
 class Convertor implements ConvertorInterface
 {
-    /**
-     * @var Config
-     */
-    private $config;
-
-    /**
-     * @var SourceImageFactory
-     */
-    private $sourceImageFactory;
-
-    /**
-     * @var File
-     */
-    private $imageFile;
-
-    /**
-     * @var ConvertWrapper
-     */
-    private $convertWrapper;
-
-    /**
-     * @var FileReadFactory
-     */
-    private $fileReadFactory;
-
-    /**
-     * Convertor constructor.
-     * @param Config $config
-     * @param SourceImageFactory $sourceImageFactory
-     * @param File $imageFile
-     * @param ConvertWrapper $convertWrapper
-     * @param FileReadFactory $fileReadFactory
-     */
     public function __construct(
-        Config $config,
-        SourceImageFactory $sourceImageFactory,
-        File $imageFile,
-        ConvertWrapper $convertWrapper,
-        FileReadFactory $fileReadFactory
+        private Config $config,
+        private TargetImageFactory $targetImageFactory
     ) {
-        $this->config = $config;
-        $this->sourceImageFactory = $sourceImageFactory;
-        $this->imageFile = $imageFile;
-        $this->convertWrapper = $convertWrapper;
-        $this->fileReadFactory = $fileReadFactory;
     }
 
-    /**
-     * @param string $imageUrl
-     * @return SourceImage
-     * @throws ConvertorException
-     * @deprecated Use getSourceImage() instead
-     */
-    public function convertByUrl(string $imageUrl): SourceImage
-    {
-        return $this->getSourceImage($imageUrl);
-    }
-
-    /**
-     * @param string $imageUrl
-     * @return SourceImage
-     * @throws ConvertorException
-     */
-    public function getSourceImage(string $imageUrl): SourceImage
+    public function convertImage(Image $image): Image
     {
         if (!$this->config->enabled()) {
             throw new ConvertorException('AVIF conversion is not enabled');
         }
 
-        $avifUrl = $this->imageFile->convertSuffix($imageUrl, '.avif');
-        $result = $this->convert($imageUrl, $avifUrl);
-
-        if (!$result && !$this->urlExists($avifUrl)) {
-            throw new ConvertorException('AVIF URL "' . $avifUrl . '" does not exist');
+        $gdImage = $image->getGdImage();
+        if (false === $gdImage instanceof GdImage) {
+            throw new ConvertorException('Unable to convert image to GD image');
         }
 
-        return $this->sourceImageFactory->create(['url' => $avifUrl, 'mimeType' => 'image/avif']);
-    }
-
-    /**
-     * @param string $sourceImageUrl
-     * @param string $destinationImageUrl
-     * @return bool
-     * @throws ConvertorException
-     */
-    public function convert(string $sourceImageUrl, string $destinationImageUrl): bool
-    {
-        $sourceImageFilename = $this->imageFile->resolve($sourceImageUrl);
-        $destinationImageFilename = $this->imageFile->resolve($destinationImageUrl);
-
-        if (!$this->needsConversion($sourceImageFilename, $destinationImageFilename)) {
-            return false;
+        if (false === function_exists('imageavif')) {
+            throw new ConvertorException('PHP does not support AVIF');
         }
 
-        if (!$this->config->enabled()) {
-            throw new ConvertorException('AVIF conversion is not enabled');
-        }
+        $avifImage = $this->targetImageFactory->create($image, 'avif');
 
         try {
-            $this->convertWrapper->convert($sourceImageFilename, $destinationImageFilename);
-        } catch (\Exception $e) {
-            throw new ConvertorException($destinationImageFilename . ': ' . $e->getMessage());
+            imageavif($gdImage, $avifImage->getPath());
+        } catch (Throwable $exception) {
+            throw new ConvertorException($exception->getMessage());
         }
 
-        return true;
-    }
-
-    /**
-     * @param string $sourceImageFilename
-     * @param string $destinationImageFilename
-     * @return bool
-     * @throws NotFoundException
-     */
-    private function needsConversion(string $sourceImageFilename, string $destinationImageFilename): bool
-    {
-        if (!$this->fileExists($sourceImageFilename)) {
-            return false;
-        }
-
-        if (!$this->fileExists($destinationImageFilename)) {
-            return true;
-        }
-
-        if ($this->imageFile->isNewerThan($destinationImageFilename, $sourceImageFilename)) {
-            return false;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $url
-     * @return bool
-     */
-    public function urlExists(string $url): bool
-    {
-        $filePath = $this->imageFile->resolve($url);
-        if ($this->fileExists($filePath)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * @param $filePath
-     * @return bool
-     */
-    private function fileExists($filePath): bool
-    {
-        try {
-            $fileRead = $this->fileReadFactory->create($filePath, 'file');
-            return (bool)$fileRead->readAll();
-        } catch (FileSystemException $fileSystemException) {
-            return false;
-        }
+        return $avifImage;
     }
 }
